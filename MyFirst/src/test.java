21b6fd230ef751e1fb0cd75bc43c402de3150f52
yuwei <ywyw1234@163.com>
2018-03-14 17:10:12 +0800

    test bean name和phone属性添加set get

--- MyFirst/src/test.java
+++ MyFirst/src/test.java
@@ -5,4 +5,20 @@ public class test {
     private String name;
 
     private String  phone;
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
